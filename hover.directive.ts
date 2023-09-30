a981b61f6ca773ca500827562bcbee9f2d408a72
Antonio <tony@email.com>
2023-09-30 12:21:10 -0600

    ok

--- hover.directive.ts
+++ hover.directive.ts
@@ -10,6 +10,8 @@ export class HoverDirective {
   @HostListener('mouseenter') onmouseover(){
     this.renderer.setStyle(this.element.nativeElement, 'margin', '5px 10px'); // 4
     this.renderer.setStyle(this.element.nativeElement, 'padding', '30px 30px'); // 22 feat 3, camb en rem
+    this.renderer.setStyle(this.element.nativeElement, 'margin', '5px 10px');
+    this.renderer.setStyle(this.element.nativeElement, 'padding', '30px 30px'); // 223 feat 3, camb en rem
   }
   @HostListener('mouseleave') onmouseout(){
     this.renderer.setStyle(this.element.nativeElement, 'margin', '10px 20px'); // con f
